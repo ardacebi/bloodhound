550cf529590717d1b38cb2e2b099baf5b15a8f22
Arda Çebi <ardacebi1@gmail.com>
2020-02-23 16:57:30 +0300

    notification modifications

--- lib/settings/item_notifications.dart
+++ lib/settings/item_notifications.dart
@@ -24,8 +24,92 @@ class _SettingsItemNotificationsState extends State<SettingsItemNotifications> {
                   onChanged: (value) {
                     setState(() {});
                   }),
-              title: Text('Account Privacy'),
-              subtitle: Text('When your account privacy is on, only approved friends will be able to see your friends and your meetups.'),
+              title: Text('Push Notifications', style: TextStyle(fontWeight: FontWeight.bold)),
+              subtitle: Text('Master switch to all notifications.'),
+              onTap: () {},
+            ),
+            ListTile(
+              trailing: new Switch(
+                  value: true,
+                  activeColor: Colors.pink,
+                  activeTrackColor: Colors.pinkAccent,
+                  onChanged: (value) {
+                    setState(() {});
+                  }),
+              title: Text('Comments and meetups mentioning you'),
+              subtitle: Text('@arda mentioned you in a meetup'),
+              onTap: () {},
+            ),
+            ListTile(
+              trailing: new Switch(
+                  value: true,
+                  activeColor: Colors.pink,
+                  activeTrackColor: Colors.pinkAccent,
+                  onChanged: (value) {
+                    setState(() {});
+                  }),
+              title: Text('Friend requests'),
+              subtitle: Text('@arda sent you a friend request'),
+              onTap: () {},
+            ),
+            ListTile(
+              trailing: new Switch(
+                  value: true,
+                  activeColor: Colors.pink,
+                  activeTrackColor: Colors.pinkAccent,
+                  onChanged: (value) {
+                    setState(() {});
+                  }),
+              title: Text('New followers'),
+              subtitle: Text('@arda started following you'),
+              onTap: () {},
+            ),
+            ListTile(
+              trailing: new Switch(
+                  value: true,
+                  activeColor: Colors.pink,
+                  activeTrackColor: Colors.pinkAccent,
+                  onChanged: (value) {
+                    setState(() {});
+                  }),
+              title: Text('Accepted follow requests'),
+              subtitle: Text('@arda accepted your following request'),
+              onTap: () {},
+            ),
+            ListTile(
+              trailing: new Switch(
+                  value: true,
+                  activeColor: Colors.pink,
+                  activeTrackColor: Colors.pinkAccent,
+                  onChanged: (value) {
+                    setState(() {});
+                  }),
+              title: Text('Meetup planning groups'),
+              subtitle: Text("@arda added you to a meetup group"),
+              onTap: () {},
+            ),
+            ListTile(
+              trailing: new Switch(
+                  value: true,
+                  activeColor: Colors.pink,
+                  activeTrackColor: Colors.pinkAccent,
+                  onChanged: (value) {
+                    setState(() {});
+                  }),
+              title: Text('Support requests'),
+              subtitle: Text('Your support request from 23.02 was updated'),
+              onTap: () {},
+            ),
+            ListTile(
+              trailing: new Switch(
+                  value: true,
+                  activeColor: Colors.pink,
+                  activeTrackColor: Colors.pinkAccent,
+                  onChanged: (value) {
+                    setState(() {});
+                  }),
+              title: Text('Bloodhound notifications'),
+              subtitle: Text('New update! Make sure to try out this'),
               onTap: () {},
             ),
             
